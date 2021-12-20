6287a4c1b65830e9b4e16b0544af6b69f574e6f8
unknown <neetamourya24@gmail.com>
2021-12-20 16:31:16 +0530

    [Neeta mourya] find max in float

--- src/main/java/com/bridgelabz/findmaximumproblemuusinggenerics/Generics.java
+++ src/main/java/com/bridgelabz/findmaximumproblemuusinggenerics/Generics.java
@@ -28,12 +28,31 @@ public class Generics <T extends Comparable <T>>{
 	System.out.println(max);
 	return max;// return the largest object
 	}
+	public T maximumToFloat() {
+		return Generics.maximumToFloat(x,y,z);
+	}
+	//determines the largest of three comparable objects
+	public static <T extends Comparable<T>> T maximumToFloat(T x, T y, T z) {
+		T max=x; //assume w is initialy the largest
+	if (y.compareTo(max)>0) {
+		max=y; //y is the largest so far
+	}
+	if (z.compareTo(max)>0) {
+		max=z; //z is the largest so far
+	}
+	printMax(x,y,z,max);
+	System.out.println(max);
+	return max;// return the largest object
+	}
 	public static <T>void printMax(T x, T y, T z, T max){
 	System.out.printf("Max of %s ,%s and %s is %s\n",x ,y ,z,max);
 	}
 public static void main(String[] args) {
 	System.out.println("Welcome to Generic maximum test");
 	Integer wInt=1, xInt=2,yInt=3,zInt=4;
+	Float wf1=0.1f, xf1=1.1f,yf1=1.2f,zf1=1.3f;
 	new Generics<>(xInt, yInt, zInt).maximumToInt();	
+	new Generics<>(xf1, yf1, zf1).maximumToFloat();
+
 }
 }
