15118e3c4beb05bfa667a106e0835e7132e30b3b
unknown <neetamourya24@gmail.com>
2021-12-20 16:57:11 +0530

    [Neeta mourya] refactor all three method in one method

--- src/main/java/com/bridgelabz/findmaximumproblemuusinggenerics/Generics.java
+++ src/main/java/com/bridgelabz/findmaximumproblemuusinggenerics/Generics.java
@@ -12,38 +12,6 @@ public class Generics <T extends Comparable <T>>{
 		this.y = y;
 		this.z = z;
 	}
-	public T maximumToInt() {
-		return Generics.maximumToInt(x,y,z);
-	}
-	//determines the largest of three comparable objects
-	public static <T extends Comparable<T>> T maximumToInt(T x, T y, T z) {
-		T max=x; //assume x is initialy the largest
-	if (y.compareTo(max)>0) {
-		max=y; //y is the largest so far
-	}
-	if (z.compareTo(max)>0) {
-		max=z; //z is the largest so far
-	}
-	printMax(x,y,z,max);
-	System.out.println(max);
-	return max;// return the largest object
-	}
-	public T maximumToFloat() {
-		return Generics.maximumToFloat(x,y,z);
-	}
-	//determines the largest of three comparable objects
-	public static <T extends Comparable<T>> T maximumToFloat(T x, T y, T z) {
-		T max=x; //assume x is initialy the largest
-	if (y.compareTo(max)>0) {
-		max=y; //y is the largest so far
-	}
-	if (z.compareTo(max)>0) {
-		max=z; //z is the largest so far
-	}
-	printMax(x,y,z,max);
-	System.out.println(max);
-	return max;// return the largest object
-	}
 	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
 	    T max = x; // assume x is initially the largest
 
@@ -55,18 +23,11 @@ public class Generics <T extends Comparable <T>>{
 
 	    return max; // returns the largest object
 	  } // end method maximum
-	public static <T>void printMax(T x, T y, T z, T max){
-	System.out.printf("Max of %s ,%s and %s is %s\n",x ,y ,z,max);
-	}
-public static void main(String[] args) {
-	System.out.println("Welcome to Generic maximum test");
-	Integer wInt=1, xInt=2,yInt=3,zInt=4;
-	Float wf1=0.1f, xf1=1.1f,yf1=1.2f,zf1=1.3f;
-	String xStr="Apple", yStr="Peach", zStr= "Banana";
-	new Generics<>(xInt, yInt, zInt).maximumToInt();	
-	new Generics<>(xf1, yf1, zf1).maximumToFloat();
-	System.out.printf("Maximum of %s, %s and %s is %s\n", "Apple", "Peach", "Banana", maximum(
-	        "Apple", "Peach", "Banana"));
 
-}
-}
+	  public static void main(String args[]) {
+	    System.out.printf("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
+	    System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,8.8, 7.7));
+	    System.out.printf("Maximum of %s, %s and %s is %s\n", "Apple", "Peach", "Banana", maximum(
+	        "Apple", "Peach", "Banana"));
+	  }
+	}
