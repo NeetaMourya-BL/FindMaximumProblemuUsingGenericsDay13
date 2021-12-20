d01a7a896436c136a4a27a520c0ecd97e645a980
unknown <neetamourya24@gmail.com>
2021-12-20 16:27:47 +0530

    [Neeta mourya] find max in integer

---
+++ src/main/java/com/bridgelabz/findmaximumproblemuusinggenerics/Generics.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.findmaximumproblemuusinggenerics;
+public class Generics <T extends Comparable <T>>{
+	T x,y,z;
+	/**
+	 * @param x
+	 * @param y
+	 * @param z
+	 */
+	public Generics(T x, T y, T z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public T maximumToInt() {
+		return Generics.maximumToInt(x,y,z);
+	}
+	//determines the largest of three comparable objects
+	public static <T extends Comparable<T>> T maximumToInt(T x, T y, T z) {
+		T max=x; //assume w is initialy the largest
+	if (y.compareTo(max)>0) {
+		max=y; //y is the largest so far
+	}
+	if (z.compareTo(max)>0) {
+		max=z; //z is the largest so far
+	}
+	printMax(x,y,z,max);
+	System.out.println(max);
+	return max;// return the largest object
+	}
+	public static <T>void printMax(T x, T y, T z, T max){
+	System.out.printf("Max of %s ,%s and %s is %s\n",x ,y ,z,max);
+	}
+public static void main(String[] args) {
+	System.out.println("Welcome to Generic maximum test");
+	Integer wInt=1, xInt=2,yInt=3,zInt=4;
+	new Generics<>(xInt, yInt, zInt).maximumToInt();	
+}
+}
